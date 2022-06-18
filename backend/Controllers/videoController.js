7aa52f1e374dc599b82a1544d5d2d462f67a421f
priyanksinha7 <66839916+priyanksinha7@users.noreply.github.com>
2022-06-18 23:16:47 +0530

    Add files via upload

--- backend/Controllers/videoController.js
+++ backend/Controllers/videoController.js
@@ -4,6 +4,7 @@ const catchAsyncErrors=require("../middleware/catchAsyncErrors");
 const ffmpeg=require("ffmpeg");
 const { exec } = require("child_process");
 //CREATE video
+
 exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
 {
 
@@ -11,6 +12,7 @@ exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
   
   //configuring inputs
   let temp_input=req.body.myVideo;
+
   if(!temp_input)
   {
     return next(new ErrorHandler("Invalid video!",400));
@@ -20,7 +22,6 @@ exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
    const nu=Math.floor(Math.random()*1000);
     var out=`W:/output${nu}.yuv`;
     const conv_type=req.body.myQuality;
-  
      
 
 
@@ -49,7 +50,7 @@ exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
         {
 
 
-          //writing on command line
+         //writing on command line
 
 
 
@@ -88,6 +89,7 @@ exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
       console.log(e.msg);
     }
     }
+  })
     
     //fmpeg version end
 
@@ -97,107 +99,105 @@ exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
 
 
 
-    //pnsr calc
-    if(conv_type==='PSNR')
-    {
-      try {
-        var process = new ffmpeg(input);
-        process.then(function (video) {
-          video.setVideoFormat('mp4').save(out).then(()=>
-          {
+  //   //pnsr calc
+  //   if(conv_type==='PSNR')
+  //   {
+  //     try {
+  //       var process = new ffmpeg(input);
+  //       process.then(function (video) {
+  //         video.setVideoFormat('mp4').save(out).then(()=>
+  //         {
   
   
-            //writing on command line
+  //           //writing on command line
   
   
   
-            exec(`ffmpeg -i ${input} -i ${out} -lavfi "[0][1]libvmaf=log_path='./logs/psnr_logfile${nu}.json':log_fmt=json:psnr=1" -f null -`, (error, stdout, stderr) => {
-              if (error) {
-                  console.log(`error: ${error.message}`);
-                  return;
-              }
-              if (stderr) {
-                //  console.log(`stderr: ${stderr}`);
-                  return;
-              }
-              //console.log(`stdout: ${stdout}`);
-            })
-             .on('close',()=>
-            {
-              const log=require(`../../logs/psnr_logfile${nu}.json`);
-            const payload=log.pooled_metrics.psnr_y;
-            payload.max=Math.floor(payload.max*100)/100;
-            payload.min=Math.floor(payload.min*100)/100;
-            payload.mean=Math.floor(payload.mean*100)/100;
-            res.status(201).json(
-              {
-                success: true,
-                payload
-              }
-            )
-            })
+  //           exec(`ffmpeg -i ${input} -i ${out} -lavfi "[0][1]libvmaf=log_path='./logs/psnr_logfile${nu}.json':log_fmt=json:psnr=1" -f null -`, (error, stdout, stderr) => {
+  //             if (error) {
+  //                 console.log(`error: ${error.message}`);
+  //                 return;
+  //             }
+  //             if (stderr) {
+  //               //  console.log(`stderr: ${stderr}`);
+  //                 return;
+  //             }
+  //             //console.log(`stdout: ${stdout}`);
+  //           })
+  //            .on('close',()=>
+  //           {
+  //             const log=require(`../../logs/psnr_logfile${nu}.json`);
+  //           const payload=log.pooled_metrics.psnr_y;
+  //           payload.max=Math.floor(payload.max*100)/100;
+  //           payload.min=Math.floor(payload.min*100)/100;
+  //           payload.mean=Math.floor(payload.mean*100)/100;
+  //           res.status(201).json(
+  //             {
+  //               success: true,
+  //               payload
+  //             }
+  //           )
+  //           })
             
-          })
-        }, function (err) {
-          console.log('Error: ' + err);
-        });
-      } catch (e) {
-        console.log(e.code);
-        console.log(e.msg);
-      }
-    }
+  //         })
+  //       }, function (err) {
+  //         console.log('Error: ' + err);
+  //       });
+  //     } catch (e) {
+  //       console.log(e.code);
+  //       console.log(e.msg);
+  //     }
+  //   }
 
 
 
 
-   if(conv_type==='SSIM')
-   {
-    try {
-      var process = new ffmpeg(input);
-      process.then(function (video) {
-        video.setVideoFormat('mp4').save(out).then(()=>
-        {
+  //  if(conv_type==='SSIM')
+  //  {
+  //   try {
+  //     var process = new ffmpeg(input);
+  //     process.then(function (video) {
+  //       video.setVideoFormat('mp4').save(out).then(()=>
+  //       {
 
 
-          //writing on command line
+  //         //writing on command line
 
 
 
-          exec(`ffmpeg -i ${input} -i ${out} -lavfi "[0][1]libvmaf=log_path='./logs/ssim_logfile${nu}.json':log_fmt=json:ssim=1" -f null -`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-              //  console.log(`stderr: ${stderr}`);
-                return;
-            }
-            //console.log(`stdout: ${stdout}`);
-          })
-           .on('close',()=>
-          {
-            const log=require(`../../logs/ssim_logfile${nu}.json`);
-          const payload=log.pooled_metrics.float_ssim;
-          payload.max=Math.floor(payload.max*100)/100;
-          payload.min=Math.floor(payload.min*100)/100;
-          payload.mean=Math.floor(payload.mean*100)/100;
-          res.status(201).json(
-            {
-              success: true,
-              payload
-            }
-          )
-          })
+  //         exec(`ffmpeg -i ${input} -i ${out} -lavfi "[0][1]libvmaf=log_path='./logs/ssim_logfile${nu}.json':log_fmt=json:ssim=1" -f null -`, (error, stdout, stderr) => {
+  //           if (error) {
+  //               console.log(`error: ${error.message}`);
+  //               return;
+  //           }
+  //           if (stderr) {
+  //             //  console.log(`stderr: ${stderr}`);
+  //               return;
+  //           }
+  //           //console.log(`stdout: ${stdout}`);
+  //         })
+  //          .on('close',()=>
+  //         {
+  //           const log=require(`../../logs/ssim_logfile${nu}.json`);
+  //         const payload=log.pooled_metrics.float_ssim;
+  //         payload.max=Math.floor(payload.max*100)/100;
+  //         payload.min=Math.floor(payload.min*100)/100;
+  //         payload.mean=Math.floor(payload.mean*100)/100;
+  //         res.status(201).json(
+  //           {
+  //             success: true,
+  //             payload
+  //           }
+  //         )
+  //         })
           
-        })
-      }, function (err) {
-        console.log('Error: ' + err);
-      });
-    } catch (e) {
-      console.log(e.code);
-      console.log(e.msg);
-    }
-   }
+  //       })
+  //     }, function (err) {
+  //       console.log('Error: ' + err);
+  //     });
+  //   } catch (e) {
+  //     console.log(e.code);
+  //     console.log(e.msg);
 
 
 
@@ -232,7 +232,6 @@ exports.uploadVideo=catchAsyncErrors(async(req,res,next)=>
 
 
 
-});
 
 
 
